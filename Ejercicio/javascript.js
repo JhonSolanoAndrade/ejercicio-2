3b09412a8bfb996592cda89aa2846b90c8466719
jhon solano <jhonsolano1@hotmail.com>
2020-05-06 14:54:58 -0500

    avanze en la rama nueva

--- Ejercicio/javascript.js
+++ Ejercicio/javascript.js
@@ -5,6 +5,10 @@ function ejecuta()
     ALERT ("PASO NUMERO 2")
     ALERT ("PASO NUMERO 3 web")
     ALERT ("PASO NUMERO 4 web")
+    ALERT ("PASO NUMERO 5 rama nueva")
+    ALERT ("PASO NUMERO 5 rama nueva")
+    ALERT ("PASO NUMERO 5 rama nueva")
+    ALERT ("PASO NUMERO 5 rama nueva")
 
 }
 
