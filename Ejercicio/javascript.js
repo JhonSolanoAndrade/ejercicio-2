62b0dc79fb1a0433fb9a67575ae259ec87c23d19
jhon solano <jhonsolano1@hotmail.com>
2020-05-06 15:03:38 -0500

    paso 6 en local

--- Ejercicio/javascript.js
+++ Ejercicio/javascript.js
@@ -5,6 +5,10 @@ function ejecuta()
     ALERT ("PASO NUMERO 2")
     ALERT ("PASO NUMERO 3 web")
     ALERT ("PASO NUMERO 4 web")
+    ALERT ("PASO NUMERO 6 local")
+    ALERT ("PASO NUMERO 6 local")
+    ALERT ("PASO NUMERO 6 local")
+    ALERT ("PASO NUMERO 6 local")
 
 }
 
